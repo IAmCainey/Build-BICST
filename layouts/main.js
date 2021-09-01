ff494e5e4d67465b2207cd63f8fedf9f867ad4fb
Paul Caine <paulcaine@tutanota.com>
2021-09-01 21:58:31 +0100

    imported scss

--- layouts/main.js
+++ layouts/main.js
@@ -2,13 +2,18 @@
 import TopNav from '../components/topnav'
 import Footer from '../components/footer'
 
+// Styling
+import style from '../styles/layout.module.scss'
+
 //styling
 
 const MainLayOut = props => {
     return (
         <div>
             <TopNav />
-            {props.children}
+            <div className={style.container}>
+                {props.children}
+            </div>
             <Footer />
         </div>
     )
