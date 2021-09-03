866a813fdbd23d2f8c10902c4cb5996d2993ccdc
Paul Caine <paulcaine@tutanota.com>
2021-09-03 10:17:20 +0100

    added hire page

---
+++ pages/the-hub/hire.js
@@ -0,0 +1,12 @@
+// Styling
+import Layout from '../../layouts/main'
+
+const bookings = () => {
+    return (
+        <Layout>
+            <h1>hub hire</h1>
+        </Layout>
+    )
+}
+
+export default bookings
