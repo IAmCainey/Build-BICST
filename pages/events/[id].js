2906676c0871a1601f391e0399b7c497c035ae15
Paul Caine <paulcaine@tutanota.com>
2021-12-10 14:06:39 +0000

    Added supabase for events

---
+++ pages/events/[id].js
@@ -0,0 +1,47 @@
+// Supabase
+import { supabase } from "../../utils/supabase"
+
+// Components
+import Head from 'next/head'
+import Link from 'next/link'
+
+// Styling
+import Layout from '../../layouts/main'
+
+const EventDetails = ({ event }) => {
+    return (
+        <Layout>
+            <h1>{event.title}</h1>
+            <strong>{event.date} | {event.time}</strong>
+            <p>
+                {event.body}
+            </p>
+        </Layout>
+    )
+}
+
+export const getStaticPaths = async () => {
+    const { data: events } = await supabase.from('event').select('id')
+
+    const paths = events.map(({ id }) => ({
+        params: {
+            id: id.toString()
+        }
+    }))
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async ({ params: { id } }) => {
+    const { data: event } = await supabase.from('event').select('*').eq('id', id).single()
+
+    return {
+        props: {
+            event
+        }
+    }
+}
+
+export default EventDetails
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,9 @@
+// Suabase
+import { supabase } from '../../utils/supabase'
+
 // Components
 import Head from 'next/head'
+import Link from 'next/link'
 
 // Styling
 import Layout from '../../layouts/main'
@@ -8,13 +12,34 @@ const title = 'BICST - Hub Events'
 const description = 'TBarrow Island Communiy Sports Trust events.'
 
 
-export default function Events() {
+export default function Events({ events }) {
+    console.log({ events })
     return (
         <Layout>
             <Head>
                 <title>{title}</title>
             </Head>
             <h2>Events</h2>
+
+            <div>
+                {events.map((event) => (
+                    <Link key={event.id} href={`/events/${event.id}`}>
+                        <a>
+                            {event.title}
+                        </a>
+                    </Link>
+                ))}
+            </div>
         </Layout>
     )
+}
+
+export const getStaticProps = async () => {
+    const { data: events } = await supabase.from('event').select('*')
+
+    return {
+        props: {
+            events,
+        }
+    }
 }
