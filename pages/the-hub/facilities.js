19f50acc6f6585a65701b9e77f6276f74bf372ce
Paul Caine <paulcaine@tutanota.com>
2022-02-01 06:18:06 +0000

    added gallery

--- pages/the-hub/facilities.js
+++ pages/the-hub/facilities.js
@@ -1,11 +1,23 @@
 // Components
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 
 
 // Styling
 import Layout from '../../layouts/main'
+import style from '../../styles/facilities.module.scss'
 
-const pageTitle = `The Hubs Fasilities`
+// Images
+import One from '../../public/images/facilities/IMG_0830.JPG'
+import Two from '../../public/images/facilities/IMG_0835.JPG'
+import Three from '../../public/images/facilities/IMG_0839.JPG'
+import Four from '../../public/images/facilities/IMG_0844.JPG'
+import Five from '../../public/images/facilities/IMG_0847.JPG'
+import Six from '../../public/images/facilities/IMG_0850.JPG'
+import Seven from '../../public/images/facilities/IMG_0856.JPG'
+
+const pageTitle = `The Hubs Facilities`
 
 const aboutTheHub = () => {
     return (
@@ -15,6 +27,63 @@ const aboutTheHub = () => {
             </Head>
             <section id="about">
                 <h1>fasilities</h1>
+            </section>
+
+            <section className={style.gallery}>
+                <div className={style.galleryItem}>
+                    <Image
+                        className={style.galleryImg}
+                        src={One}
+                        alt='this is an alt tag'
+                    />
+                </div>
+                <div className={style.galleryItem}>
+                    <Image
+                        className={style.galleryImg}
+                        src={Two}
+                        alt='this is an alt tag'
+                    />
+                </div>
+                <div className={style.galleryItem}>
+                    <Image
+                        className={style.galleryImg}
+                        src={Three}
+                        alt='this is an alt tag'
+                    />
+                </div>
+                <div className={style.galleryItem}>
+                    <Image
+                        className={style.galleryImg}
+                        src={Four}
+                        alt='this is an alt tag'
+                    />
+                </div>
+                <div className={style.galleryItem}>
+                    <Image
+                        className={style.galleryImg}
+                        src={Five}
+                        alt='this is an alt tag'
+                    />
+                </div>
+                <div className={style.galleryItem}>
+                    <Image
+                        className={style.galleryImg}
+                        src={Six}
+                        alt='this is an alt tag'
+                    />
+                </div>
+                <div className={style.galleryItem}>
+                    <Image
+                        className={style.galleryImg}
+                        src={Seven}
+                        alt='this is an alt tag'
+                    />
+                </div>
+            </section>
+
+            <section className='my-md'>
+                <h1>welcome</h1>
+
             </section>
         </Layout>
     )
