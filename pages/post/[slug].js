2c24daac5030873a8e1ba7f50c425196377f8e69
Paul Caine <paulcaine@tutanota.com>
2022-02-01 10:09:59 +0000

    fix

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,97 +1,74 @@
-// // Components
-// import groq from 'groq'
-// import imageUrlBuilder from '@sanity/image-url'
-// import BlockContent from '@sanity/block-content-to-react'
-// import Image from 'next/image'
+import groq from 'groq'
+import imageUrlBuilder from '@sanity/image-url'
+import BlockContent from '@sanity/block-content-to-react'
+import client from '../../client'
 
-// import client from '../../client'
+function urlFor(source) {
+    return imageUrlBuilder(client).image(source)
+}
 
-// // Styling
-// import Layout from '../../layouts/main'
-
-// function urlFor(source) {
-//     return imageUrlBuilder(client).image(source)
-// }
-
-// const Post = ({ post }) => {
-//     const {
-//         title = `null`,
-//         name = `null`,
-//         categories,
-//         authorImage,
-//         body = []
-//     } = post
-//     return (
-//         <Layout>
-
-//             <article>
-//                 <h1>{title}</h1>
-//                 <span>By {name}</span>
-//                 {categories && (
-//                     <ul>
-//                         Posted in
-//                         {categories.map(category => <li key={category}>{category}</li>)}
-//                     </ul>
-//                 )}
-//                 {authorImage && (
-//                     <div>
-//                         <Image
-//                             src={urlFor(authorImage)
-//                                 .width(200)
-//                                 .url()}
-//                             alt="{name}"
-//                             width={200}
-//                             height={150}
-//                         />
-//                     </div>
-//                 )}
-//                 <BlockContent
-//                     blocks={body}
-//                     imageOptions={{ w: 320, h: 240, fit: 'max' }}
-//                     {...client.config()}
-//                 />
-
-
-//             </article>
-//         </Layout>
-//     )
-// }
-
-// const query = groq`*[_type == "post" && slug.current == $slug][0]{
-//   title,
-//   "name": author->name,
-//   "categories": categories[]->title,
-//   "authorImage": author->image,
-//   body
-// }`
-// export async function getStaticPaths() {
-//     const paths = await client.fetch(
-//         groq`*[_type == "post" && defined(slug.current)][].slug.current`
-//     )
-
-//     return {
-//         paths: paths.map((slug) => ({ params: { slug } })),
-//         fallback: true,
-//     }
-// }
-
-// export async function getStaticProps(context) {
-//     // It's important to default the slug so that it doesn't return "undefined"
-//     const { slug = "" } = context.params
-//     const post = await client.fetch(query, { slug })
-//     return {
-//         props: {
-//             post
-//         }
-//     }
-// }
-// export default Post
-
-
-export default function NameHere() {
+const Post = ({ post }) => {
+    const {
+        title = 'Missing title',
+        name = 'Missing name',
+        categories,
+        authorImage,
+        body = []
+    } = post
     return (
-        <div>
-            <h1>welcome</h1>
-        </div>
+        <article>
+            <h1>{title}</h1>
+            <span>By {name}</span>
+            {categories && (
+                <ul>
+                    Posted in
+                    {categories.map(category => <li key={category}>{category}</li>)}
+                </ul>
+            )}
+            {authorImage && (
+                <div>
+                    <img
+                        src={urlFor(authorImage)
+                            .width(50)
+                            .url()}
+                    />
+                </div>
+            )}
+            <BlockContent
+                blocks={body}
+                imageOptions={{ w: 320, h: 240, fit: 'max' }}
+                {...client.config()}
+            />
+        </article>
     )
 }
+
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title,
+  "authorImage": author->image,
+  body
+}`
+export async function getStaticPaths() {
+    const paths = await client.fetch(
+        groq`*[_type == "post" && defined(slug.current)][].slug.current`
+    )
+
+    return {
+        paths: paths.map((slug) => ({ params: { slug } })),
+        fallback: true,
+    }
+}
+
+export async function getStaticProps(context) {
+    // It's important to default the slug so that it doesn't return "undefined"
+    const { slug = "" } = context.params
+    const post = await client.fetch(query, { slug })
+    return {
+        props: {
+            post
+        }
+    }
+}
+export default Post
