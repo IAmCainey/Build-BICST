7179d6eb54ca918e2435f7addd28ef4b1c87cba2
Paul Caine <paulcaine@tutanota.com>
2022-02-05 21:42:14 +0000

    Added sponsors component

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,15 @@
 import Layout from '../layouts/landingpage'
 import style from '../styles/landingpage.module.scss'
 import cardStyle from '../styles/cards.module.scss'
-import sponsorsStyle from '../styles/sponsors.module.scss'
 
 // Components
+import Sponsors from '../components/sponsors'
 
 
 // Profile Images
 import profileRob from '../public/images/profiles/rob.jpg'
 import profileDebbie from '../public/images/profiles/debbie.jpg'
 import profileLaura from '../public/images/profiles/laura.webp'
-
-// Sponsors
-import sponsorsCoop from '../public/images/sponsors/coop.jpg'
-import sponsorsJohnfisher from '../public/images/sponsors/johnfisher.jpg'
-import sponsorsLocaltrust from '../public/images/sponsors/localtrust.jpg'
-import sponsorsLotto from '../public/images/sponsors/lotto.png'
-import sponsorsWren from '../public/images/sponsors/wren.png'
 
 
 // Next Imports
@@ -137,73 +130,8 @@ const LandingPage = () => {
         </div>
       </section>
 
-      <section className={sponsorsStyle.sponsors}>
-        <Link href='https://www.coop.co.uk/store-finder/LA14-2SD/island-road'>
-          <a target='blank'>
-            <div className={sponsorsStyle.img}>
-              <Image
-                src={sponsorsCoop}
-                alt="The COOP Barrow Island"
-                className={sponsorsStyle.img}
-                placeholder="blur"
-                title='The COOP Barrow Island'
-              />
-            </div>
-          </a>
-        </Link>
-        <Link href='https://sirjohnfisherfoundation.org.uk/'>
-          <a target='blank'>
-            <div className={sponsorsStyle.img}>
-              <Image
-                src={sponsorsJohnfisher}
-                alt="John Fisher Foundation"
-                className={sponsorsStyle.img}
-                placeholder="blur"
-                title='John Fisher Foundation'
-              />
-            </div>
-          </a>
-        </Link>
-        <Link href='https://localtrust.org.uk/big-local/'>
-          <a target='blank'>
-            <div className={sponsorsStyle.img}>
-              <Image
-                src={sponsorsLocaltrust}
-                alt="Local Trush | Big Local"
-                className={sponsorsStyle.img}
-                placeholder="blur"
-                title='Local Trush | Big Local'
-              />
-            </div>
-          </a>
-        </Link>
-        <Link href='https://www.tnlcommunityfund.org.uk/'>
-          <a target='blank'>
-            <div className={sponsorsStyle.img}>
-              <Image
-                src={sponsorsLotto}
-                alt="Big Lottery Fund"
-                className={sponsorsStyle.img}
-                placeholder="blur"
-                title='Big Lottery Fund'
-              />
-            </div>
-          </a>
-        </Link>
-        <Link href='#'>
-          <a target='blank'>
-            <div className={sponsorsStyle.img}>
-              <Image
-                src={sponsorsWren}
-                alt="Wren, Resoucing YourProjects"
-                className={sponsorsStyle.img}
-                placeholder="blur"
-                title='Wren, Resoucing YourProjects'
-              />
-            </div>
-          </a>
-        </Link>
-      </section>
+      <Sponsors />
+
     </Layout>
   )
 }
