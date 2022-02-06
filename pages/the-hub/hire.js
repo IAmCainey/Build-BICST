853f4b41ec92537591c50fb5d88646099de42bbf
Paul Caine <paulcaine@tutanota.com>
2022-02-06 21:53:08 +0000

    fixed layout

--- pages/the-hub/hire.js
+++ pages/the-hub/hire.js
@@ -14,43 +14,34 @@ const bookings = (props) => {
             <div className='flex'>
                 <div>
                     <h1>hub hire</h1>
-                    <section>
+                    <h4>short term</h4>
 
-                        <section className='tm-lg'>
-                            <h4>short term</h4>
+                    <p>For short term bookings this is,</p>
 
-                            <p>For short term bookings this is,</p>
-                            <ul className='my-md'>
-                                <li>parties</li>
-                                <li>meeting</li>
-                                <li>events</li>
-                            </ul>
+                    <ul className='my-md'>
+                        <li>parties</li>
+                        <li>meeting</li>
+                        <li>events</li>
+                    </ul>
 
-                            <p>To book for the short term, please use our booking system starting with the booking form</p>
-                        </section>
+                    <p>To book for the short term, please use our booking system starting with the booking form</p>
 
-                        <section className='tm-lg'>
-                            <h4>long term</h4>
+                    <h4 className='tm-lg'>long term</h4>
 
-                            <p>This option is ideal for groups,</p>
+                    <p>This option is ideal for groups,</p>
 
-                            <ul className='my-md'>
-                                <li>dancers</li>
-                                <li>drama</li>
-                                <li>weekly / monthly meetups</li>
-                            </ul>
+                    <ul className='my-md'>
+                        <li>dancers</li>
+                        <li>drama</li>
+                        <li>weekly / monthly meetups</li>
+                    </ul>
 
-                            <p>To book the hall for the long term this needs to be done in person.</p>
-                        </section>
-
-                    </section>
-
-                    <section>
-                        <p>please make sure you read the following before booking the hall out!</p>
-
-                    </section>
+                    <p>To book the hall for the long term this needs to be done in person.</p>
+                    <p>please make sure you read the following before booking the hall out!</p>
                 </div>
-                <div className='align-right'>
+
+
+                <div className='tm-lg'>
                     <Link href="/the-hub/bookings">
                         <a className='btn'>Hall Hire</a>
                     </Link>
@@ -59,7 +50,7 @@ const bookings = (props) => {
                     </Link>
                 </div>
             </div>
-        </Layout>
+        </Layout >
     )
 }
 
