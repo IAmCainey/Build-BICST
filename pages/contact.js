5109ce9c9f0bd1f90abf18284281baa811d55d45
Paul Caine <paulcaine@tutanota.com>
2022-02-07 23:00:14 +0000

    added contact page

---
+++ pages/contact.js
@@ -0,0 +1,19 @@
+// Components
+import Head from 'next/head'
+import Layout from '../layouts/main'
+
+// Styling
+
+
+export default function Contact() {
+    return (
+        <Layout>
+            <Head>
+                <title>Contact Us @ BICST</title>
+            </Head>
+            <div>
+                <h1>coming soon</h1>
+            </div>
+        </Layout >
+    )
+}
