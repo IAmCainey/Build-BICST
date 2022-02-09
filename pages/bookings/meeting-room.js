a4ec9dcd479f607ea7fc4bbe8216619396b63a6d
Paul Caine <paulcaine@tutanota.com>
2022-02-09 11:38:30 +0000

    added booking pages

---
+++ pages/bookings/meeting-room.js
@@ -0,0 +1,29 @@
+// Components
+import Head from 'next/head'
+import Link from 'next/link'
+
+// Styling
+import Layout from '../../layouts/main'
+import style from '../../styles/bookings.module.scss'
+
+
+export default function MeetingRoom() {
+    return (
+        <Layout>
+            <Head>
+                <title>Bookings @ BICST</title>
+            </Head>
+
+            <div className='flex'>
+                <div>
+                    <h2>Hub Booking Form</h2>
+                    <p>please use the form only for booking the hub</p>
+                </div>
+
+                <div>
+                    <iframe src="https://koalendar.com/e/hub-booking-form?embed=true" width="100%" height="800px" frameBorder="0"></iframe>
+                </div>
+            </div>
+        </Layout>
+    )
+}
