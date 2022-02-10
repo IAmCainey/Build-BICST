a18e4a35cb59544eab72a65dad834f9a73b8a876
Paul Caine <paulcaine@tutanota.com>
2022-02-10 12:28:40 +0000

    added image to header

--- components/pageHero.js
+++ components/pageHero.js
@@ -8,9 +8,11 @@ import style from '../styles/pageHero.module.scss'
 const pageHero = (props) => {
     return (
         <section className={style.hero}>
-            <div className={style.container}>
-                <h1 className={style.title}>Barrow Island, <br />Community Sports Trust</h1>
-                <span className='light-text'>A place that brings the community together</span>
+            <div className={style.overlay}>
+                <div className={style.container}>
+                    <h1 className={style.title}>Barrow Island, <br />Community Sports Trust</h1>
+                    <span className='light-text'>A place that brings the community together</span>
+                </div>
             </div>
         </section>
     )
