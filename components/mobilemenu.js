1cc69d9ca4ba9ea00696c6d73170a1b403fbf2b5
Paul Caine <paulcaine@tutanota.com>
2022-02-10 12:13:10 +0000

    added mobile menu

--- components/mobilemenu.js
+++ components/mobilemenu.js
@@ -2,7 +2,9 @@
 import style from '../styles/mobilemenu.module.scss'
 
 // Imports
+import Link from 'next/link'
 
+// Icons
 import { GiHamburgerMenu } from 'react-icons/gi'
 
 export default function MobileMenu() {
@@ -11,8 +13,30 @@ export default function MobileMenu() {
             <div>
                 <h3 className={style.title}>BICST</h3>
             </div>
-            <div>
-                <GiHamburgerMenu className={style.burger} />
+            <div className={style.burger_2}>
+
+                <GiHamburgerMenu className={style.burger}>
+                </GiHamburgerMenu>
+
+                <div className={style.dropdown}>
+                    <div className={style.links}>
+                        <Link href='/'>
+                            <a className={style.link}>Home</a>
+                        </Link>
+                        <Link href='/gallery'>
+                            <a className={style.link}>Gallery</a>
+                        </Link>
+                        <Link href='/bookings'>
+                            <a className={style.link}>Bookings</a>
+                        </Link>
+                        <Link href='/events'>
+                            <a className={style.link}>Events</a>
+                        </Link>
+                        <Link href='/contact'>
+                            <a className={style.link}>contact</a>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
