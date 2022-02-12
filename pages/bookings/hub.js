3d761c2707abe2e7b4b403eb819f845c7145f7aa
Paul Caine <paulcaine@tutanota.com>
2022-02-12 13:47:40 +0000

    added content

--- pages/bookings/hub.js
+++ pages/bookings/hub.js
@@ -16,8 +16,33 @@ export default function Hub() {
 
             <div className='flex'>
                 <div>
-                    <h2>Hub Booking Form</h2>
-                    <p>please use the form only for booking the hub</p>
+                    <h2>Hub Booking</h2>
+                    <p>Befor booking the hub, please take the time to read and understand the terms set out by the trust.</p>
+                    <Link href='/terms'>
+                        <a>
+                            Terms & Conditions
+                        </a>
+                    </Link>
+
+                    <p>
+                        Please do not use this bookings page for the following:
+                    </p>
+                    <ul>
+                        <li>Weddings</li>
+                        <li>Christenings</li>
+                        <li>Training Days</li>
+                    </ul>
+                    <p>
+                        If your looking to book any of the above please contact us for prices.
+                        If catering is required please make sure you contact us before hand.
+                    </p>
+                    <p>If you have any questions please feel free to contact us.</p>
+
+                    <Link href='/contact'>
+                        <a>
+                            Contact Us
+                        </a>
+                    </Link>
                 </div>
 
                 <div>
