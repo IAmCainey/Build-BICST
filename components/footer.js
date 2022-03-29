146a58b9cb1bc783187cbc41e836bb467244fdca
Paul Caine <paulcaine@tutanota.com>
2022-03-29 05:16:28 +0100

    added events link

--- components/footer.js
+++ components/footer.js
@@ -1,93 +1,114 @@
 // Components
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
-import sponsorLotto from '../public/images/sponsors/lotto.png'
+import sponsorLotto from "../public/images/sponsors/lotto.png";
 
 // Styling
-import style from '../styles/footer.module.scss'
-
+import style from "../styles/footer.module.scss";
 
 // Icons
-import { AiOutlineFacebook, AiOutlineTwitter, AiOutlineMail, AiOutlinePhone } from 'react-icons/ai'
+import {
+  AiOutlineFacebook,
+  AiOutlineTwitter,
+  AiOutlineMail,
+  AiOutlinePhone,
+} from "react-icons/ai";
 
 const Footer = () => {
-    return (
-        <div>
-            <div className={style.top_footer} />
-            <footer className={style.footer}>
-                <div className={style.container}>
-                    <div className={style.item}>
-                        <Link href="/">
-                            <a className={style.link}>home</a>
-                        </Link>
-                        <Link href="/bookings">
-                            <a className={style.link}>Bookings</a>
-                        </Link>
-                        <Link href="/galleries">
-                            <a className={style.link}>Gallery</a>
-                        </Link>
-                        <Link href="/contact">
-                            <a className={style.link}>Contact Us</a>
-                        </Link>
-                    </div>
-                    <div className={style.item}>
-                        <Link href="/terms" className={style.link}>
-                            <a className={style.link}>terms</a>
-                        </Link>
-                        <Link href="/privacy">
-                            <a className={style.link}>privacy</a>
-                        </Link>
-                    </div>
-                    <div className={style.item}>
-                        <h3 className={style.heading}>contact us</h3>
-                        <p className={style.text}>Please feel free to contat Barrow Island Community Sports Trust with any questions you have.</p>
-                        <p className={style.text}>
-                            Barrow Island Community Sports Trust ltd. <br />
-                            Cavendish Park, Barrow Island, Cumbria. <br />
-                            Contact Number : <span className={style.span}>01229 820470</span> <br />
-                            <a className={style.link_2} href="mailto:bicstrust@gmail.com">bicstrust@gmail.com</a>
-                        </p>
-                        <Image
-                            className={style.lotto}
-                            src={sponsorLotto}
-                            alt="the big lottery found"
-                            placeholder="blur"
-                        />
+  return (
+    <div>
+      <div className={style.top_footer} />
+      <footer className={style.footer}>
+        <div className={style.container}>
+          <div className={style.item}>
+            <Link href="/">
+              <a className={style.link}>home</a>
+            </Link>
+            <Link href="/bookings">
+              <a className={style.link}>Bookings</a>
+            </Link>
+            <Link href="/galleries">
+              <a className={style.link}>Gallery</a>
+            </Link>
+            <Link href="/contact">
+              <a className={style.link}>Contact Us</a>
+            </Link>
+            <Link href="/events">
+              <a className={style.link}>Events</a>
+            </Link>
+          </div>
+          <div className={style.item}>
+            <Link href="/terms" className={style.link}>
+              <a className={style.link}>terms</a>
+            </Link>
+            <Link href="/privacy">
+              <a className={style.link}>privacy</a>
+            </Link>
+          </div>
+          <div className={style.item}>
+            <h3 className={style.heading}>contact us</h3>
+            <p className={style.text}>
+              Please feel free to contat Barrow Island Community Sports Trust
+              with any questions you have.
+            </p>
+            <p className={style.text}>
+              Barrow Island Community Sports Trust ltd. <br />
+              Cavendish Park, Barrow Island, Cumbria. <br />
+              Contact Number : <span className={style.span}>
+                01229 820470
+              </span>{" "}
+              <br />
+              <a className={style.link_2} href="mailto:bicstrust@gmail.com">
+                bicstrust@gmail.com
+              </a>
+            </p>
+            <Image
+              className={style.lotto}
+              src={sponsorLotto}
+              alt="the big lottery found"
+              placeholder="blur"
+            />
 
-                        <div className='social'>
-                            <Link href="https://www.facebook.com/bicstrust">
-                                <a target='blank'>
-                                    <AiOutlineFacebook className='icon-social' />
-                                </a>
-                            </Link>
-                            <Link href='https://twitter.com/bicstrust'>
-                                <a target='blank'>
-                                    <AiOutlineTwitter className='icon-social' />
-                                </a>
-                            </Link>
-                            <Link href='mailto:bicstrust@gmail.com'>
-                                <a>
-                                    <AiOutlineMail className='icon-social' />
-                                </a>
-                            </Link>
-                            <Link href='tel:01229 820470'>
-                                <a>
-                                    <AiOutlinePhone className='icon-social' />
-                                </a>
-                            </Link>
-                        </div>
-
-                    </div>
-                </div>
-                <div className={style.footer_note}>
-                    <p className={style.text}>developed by // <a className={style.link_2} href="https://paulcaine.co.uk" target="blank">Paul Caine</a>
-                    </p>
-                </div>
-            </footer>
-
+            <div className="social">
+              <Link href="https://www.facebook.com/bicstrust">
+                <a target="blank">
+                  <AiOutlineFacebook className="icon-social" />
+                </a>
+              </Link>
+              <Link href="https://twitter.com/bicstrust">
+                <a target="blank">
+                  <AiOutlineTwitter className="icon-social" />
+                </a>
+              </Link>
+              <Link href="mailto:bicstrust@gmail.com">
+                <a>
+                  <AiOutlineMail className="icon-social" />
+                </a>
+              </Link>
+              <Link href="tel:01229 820470">
+                <a>
+                  <AiOutlinePhone className="icon-social" />
+                </a>
+              </Link>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className={style.footer_note}>
+          <p className={style.text}>
+            developed by //{" "}
+            <a
+              className={style.link_2}
+              href="https://paulcaine.co.uk"
+              target="blank"
+            >
+              Paul Caine
+            </a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- components/topnav.js
+++ components/topnav.js
@@ -1,69 +1,63 @@
 // Components
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
 // Styling
-import style from '../styles/topnav.module.scss'
+import style from "../styles/topnav.module.scss";
 
 // Images
-import logo from '../public/images/logo.png'
-
+import logo from "../public/images/logo.png";
 
 const topNav = () => {
-    return (
-        <nav className={style.navbar}>
-            <div className={style.container}>
-                <div>
-                    <Link href="/">
-                        <a className={style.titleLink}>
-                            <Image
-                                src={logo}
-                                alt="Barrow Island Community Sports Trust"
-                                className={style.logo}
-                                placeholder="blur"
-                                height={75}
-                                width={250}
-                            />
-                        </a>
-                    </Link>
+  return (
+    <nav className={style.navbar}>
+      <div className={style.container}>
+        <div>
+          <Link href="/">
+            <a className={style.titleLink}>
+              <Image
+                src={logo}
+                alt="Barrow Island Community Sports Trust"
+                className={style.logo}
+                placeholder="blur"
+                height={75}
+                width={250}
+              />
+            </a>
+          </Link>
+        </div>
+        <div className={style.linkBlock}>
+          <Link href="/">
+            <a className={style.link}>home</a>
+          </Link>
+          <Link href="#">
+            <a className={`${style.link} ${style.drop}`}>
+              the hub
+              <div className={style.dropdown}>
+                <div className={style.dropdown_content}>
+                  <Link href="/galleries">
+                    <a className={style.link}>Gallories</a>
+                  </Link>
+                  <Link href="/bookings">
+                    <a className={style.link}>Bookings</a>
+                  </Link>
                 </div>
-                <div className={style.linkBlock}>
-                    <Link href="/">
-                        <a className={style.link}>
-                            home
-                        </a>
-                    </Link>
-                    <Link href="#">
-                        <a className={`${style.link} ${style.drop}`}>
-                            the hub
-                            <div className={style.dropdown}>
-                                <div className={style.dropdown_content}>
-                                    <Link href="/galleries">
-                                        <a className={style.link}>
-                                            Gallories
-                                        </a>
-                                    </Link>
-                                    <Link href="/bookings">
-                                        <a className={style.link}>
-                                            Bookings
-                                        </a>
-                                    </Link>
-                                </div>
-                            </div>
-                        </a>
-                    </Link>
-                    <Link href="/contact">
-                        <a className={style.link}>
-                            Contact Us
-                        </a>
-                    </Link>
-                    <Link href="/bookings">
-                        <a className='btn-reverse lm-md'>Book Now</a>
-                    </Link>
-                </div>
-            </div>
-        </nav>
-    )
-}
+              </div>
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a className={style.link}>Contact Us</a>
+          </Link>
+          <Link href="/events">
+            <a className={style.link}>Events</a>
+          </Link>
+          <Link href="/bookings">
+            <a className="btn-reverse lm-md">Book Now</a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default topNav
+export default topNav;
